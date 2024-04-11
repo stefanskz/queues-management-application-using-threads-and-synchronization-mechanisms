d36c230ed60eec1be1578e9f9b2d0b94cdb1f652
Your Name <stefanskz04@gmail.com>
2024-04-11 11:41:22 +0300

    Second commit!

--- src/main/java/model/Server.java
+++ src/main/java/model/Server.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Server implements Runnable {
@@ -8,21 +9,32 @@ public class Server implements Runnable {
     private AtomicInteger waitingPeriod;
 
     public Server() {
-
+        tasks = new LinkedBlockingQueue<>();
+        waitingPeriod = new AtomicInteger(0);
     }
 
     public void addTask(Task newTask) {
-
+        tasks.add(newTask);
+        this.setWaitingPeriod(this.getWaitingPeriod() + newTask.getServiceTime());
     }
 
     public void run() {
         while (true) {
-
+            //take next task from queue
+            //stop the thread for a time equal with the task's service time
+            //decrement the waitingPeriod
         }
     }
 
-    public Task[] getTasks() {
-        return null;
+    public BlockingQueue<Task> getTasks() {
+        return tasks;
     }
 
+    public int getWaitingPeriod() {
+        return this.waitingPeriod.get();
+    }
+
+    public void setWaitingPeriod(int waitingPeriod) {
+        this.waitingPeriod.set(waitingPeriod);
+    }
 }
